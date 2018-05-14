43840d9d3fc1cdeea7d67333a56cc5e8f75adc09
Denis Vlasov <denis90@gmail.com>
2018-05-14 19:53:45 +0300

    Release v1.6.0

--- main.go
+++ main.go
@@ -43,7 +43,7 @@ import (
 // It is also set by the linker when fabio
 // is built via the Makefile or the build/docker.sh
 // script to ensure the correct version nubmer
-var version = "1.7.0"
+var version = "1.6.0"
 
 var shuttingDown int32
 
